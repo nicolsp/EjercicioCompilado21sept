8f2400434d671ba3cc7250239d684396ae2c55f7
copito.123 <nicolspnicolsp@gmail.com>
2020-09-25 20:23:04 -0300

    24 setp,

--- app/src/main/java/com/example/ejerciciocompilado21sept/model/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/ejerciciocompilado21sept/model/Retrofit/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient {
     companion object {
-        private const val URL_BASE = "https://android-kotlin-fun-mars-server.appspot.com/"
+        private const val URL_BASE = "https://dog.ceo/api"
 
         fun getRetrofitCliente() : ApiInterface {
             var mRetrofit = Retrofit.Builder()
